b60ed9c49905db3024b7554930758eb157f787a4
毕云帆 <biyunfan@xiaobuke.com>
2019-10-08 17:15:50 +0800

    await test

--- modules/promiseTest.js
+++ modules/promiseTest.js
@@ -48,12 +48,14 @@ function step1( resolve , reject ) {
 // .then((n)=> step2(n))
 // .then((n)=> step2(n))
 
-function 买菜(resolve,reject) {
+function 买菜() {
   log('买菜ing')
+  return new Promise((resolve, reject)=>{
   setTimeout(function(){
     log('买菜ok')
       resolve(['西红柿','鸡蛋','油菜']);
   },3000)
+})
 }
 function 做饭(data){
   return new Promise((resolve, reject)=>{
@@ -89,17 +91,27 @@ function 电话通知我(n){
   //电话通知我后的下一步处理
   //给保姆加100块钱奖金;
   log('电话通知'+n)
+  return ('电话通知'+n)
 }
-new Promise(买菜)
-//用买好的菜做饭
-.then((买好的菜)=>{
-    return 做饭(买好的菜);
-})
-//把做好的饭送到老婆公司
-.then((做好的饭)=>{
-    return (送饭(做好的饭));
-})
-//送完饭后打电话通知我
-.then((送饭结果)=>{
-    电话通知我(送饭结果);
-})
+
+// 买菜()
+// //用买好的菜做饭
+// .then((买好的菜)=>{
+//     return 做饭(买好的菜);
+// })
+// //把做好的饭送到老婆公司
+// .then((做好的饭)=>{
+//     return (送饭(做好的饭));
+// })
+// //送完饭后打电话通知我
+// .then((送饭结果)=>{
+//     电话通知我(送饭结果);
+// })
+
+(async () => {
+  let 蔬菜 = await 买菜();
+  let 饭菜 = await 做饭(蔬菜);
+  let 送饭结果 = await 送饭(饭菜);
+  let 通知结果 = await 电话通知我(送饭结果);
+  console.log(通知结果)
+})()
